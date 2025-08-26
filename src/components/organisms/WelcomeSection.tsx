a9405ad791c380f391a3695a28c16be48543a25c
Your Name <you@example.com>
2025-08-26 15:12:09 +0330

    Readme

--- src/components/organisms/WelcomeSection.tsx
+++ src/components/organisms/WelcomeSection.tsx
@@ -7,7 +7,7 @@ interface WelcomeSectionProps {
 }
 
 export default function WelcomeSection({user}: WelcomeSectionProps) {
-    return (<div  className={"flex flex-col gap-2 justify-center items-center w-full h-[80vh]"}>
+    return (<div  className={"flex flex-col gap-2 justify-center items-center w-full min-h-[80vh]"}>
         <Alert className={"w-fit"}>
             <CheckCircle2Icon/>
             <AlertTitle>
